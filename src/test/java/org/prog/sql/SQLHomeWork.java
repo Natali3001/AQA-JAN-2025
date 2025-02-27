c87d7e6abdf8768d235174f0976c4c662954fbb3
Natali <nataliya.gorelova.2011@gmail.com>
2025-02-27 11:21:19 +0100

    Session 2: 123
    Signed-off-by: Natali <nataliya.gorelova.2011@gmail.com>

--- src/test/java/org/prog/sql/SQLHomeWork.java
+++ src/test/java/org/prog/sql/SQLHomeWork.java
@@ -11,7 +11,34 @@ package org.prog.sql;
 // - get phone price
 // - store phone name and phone price to DB
 
-public class SQLHomeWork {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class FetchPhones {
+    private static final String URL = "jdbc:mysql://localhost:3306/your_database";
+    private static final String USER = "root";
+    private static final String PASSWORD = "your_password";
 
+    public static void main(String[] args) {
+        String selectSQL = "SELECT * FROM Phones;";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(selectSQL)) {
+
+            while (rs.next()) {
+                int id = rs.getInt("PhoneId");
+                String name = rs.getString("PhoneName");
+                double price = rs.getDouble("PhonePrice");
+
+                System.out.println("ID: " + id + ", Name: " + name + ", Price: $" + price);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
